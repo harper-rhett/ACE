e1740f4057caf09dcbb81152656db93fffda9ce0
Harper Rhett <harperrhett@gmail.com>
2024-09-29 12:41:38 -0500

    Added folder dialogs

--- source/App.xaml.cs
+++ source/App.xaml.cs
@@ -10,5 +10,4 @@ namespace ACE
 	public partial class App : Application
 	{
 	}
-
 }
--- source/MainWindow.xaml.cs
+++ source/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Shapes;
 
 // Added namespaces
 using System.Diagnostics;
+using System.IO;
 
 namespace ACE
 {
@@ -22,14 +23,62 @@ namespace ACE
 			InitializeComponent();
 		}
 
-		private void TrainModelClick(object sender, RoutedEventArgs e)
+		private void SelectTrainingData_Click(object sender, RoutedEventArgs e)
+		{
+			// Initialize folder dialog
+			Microsoft.Win32.OpenFolderDialog folderDialog = new();
+			folderDialog.Multiselect = false;
+			folderDialog.Title = "Select Training Data";
+
+			// Check if selected or cancelled
+			bool? selectedFolder = folderDialog.ShowDialog();
+			if (selectedFolder == true)
+			{
+				// Load the selected folder
+				string folderName = folderDialog.SafeFolderName;
+				string folderPath = folderDialog.FolderName;
+				LoadVideos(folderName, folderPath);
+			}
+		}
+
+		private void SelectModelData_Click(object sender, RoutedEventArgs e)
 		{
 
 		}
 
-		private void RunModelClick(object sender, RoutedEventArgs e)
+		private void SelectDebrisVideos_Click(object sender, RoutedEventArgs e)
+		{
+			// Initialize folder dialog
+			Microsoft.Win32.OpenFolderDialog folderDialog = new();
+			folderDialog.Multiselect = false;
+			folderDialog.Title = "Select Debris Videos";
+
+			// Check if selected or cancelled
+			bool? selectedFolder = folderDialog.ShowDialog();
+			if (selectedFolder == true)
+			{
+				// Load the selected folder
+				string folderName = folderDialog.SafeFolderName;
+				string folderPath = folderDialog.FolderName;
+				LoadVideos(folderName, folderPath);
+			}
+		}
+
+		private void TrainModel_Click(object sender, RoutedEventArgs e)
 		{
 
 		}
-    }
+
+		private void RunModel_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void LoadVideos(string folderName, string folderPath)
+		{
+			string[] filePaths = Directory.GetFiles(folderPath, "*.mp4", SearchOption.AllDirectories);
+			Debug.Print($"Loading {filePaths.Length} videos at {folderPath}");
+
+		}
+	}
 }
